42296f940f210c86d771d89f5cfd5ca6d7fa52ed
ISHII Tsuubito <ishii@buffer-renaiss.com>
2014-12-15 16:37:57 +0900

    initial

---
+++ ofxFunctional_08x.h
@@ -0,0 +1,17 @@
+//
+//  ofxFunctional_08x.h
+//
+//  Created by ISHII 2bit on 2014/12/15.
+//
+//
+
+#pragma once
+
+#include <tr1/functional>
+#include <tr1/tuple>
+
+namespace std {
+    using tr1::function;
+    using tr1::bind;
+    using tr1::tuple;
+};
